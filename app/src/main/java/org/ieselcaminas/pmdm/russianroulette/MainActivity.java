ffe0336d12e2fda6184adc9f456e9f590ecd148a
Adrian <adrian_ldt_95@hotmail.com>
2018-09-18 16:15:51 +0200

    Initial Commit RussianRoulette

---
+++ app/src/main/java/org/ieselcaminas/pmdm/russianroulette/MainActivity.java
@@ -0,0 +1,79 @@
+package org.ieselcaminas.pmdm.russianroulette;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button randomButton;
+    Button[] buttons = new Button[6];
+    TextView textView;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        randomButton = createRandomButton();
+
+        buttons[0] = findViewById(R.id.button1);
+        buttons[1] = findViewById(R.id.button2);
+        buttons[2] = findViewById(R.id.button3);
+        buttons[3] = findViewById(R.id.button4);
+        buttons[4] = findViewById(R.id.button5);
+        buttons[5] = findViewById(R.id.button6);
+
+        Button buttonReload = findViewById(R.id.buttonReload);
+
+        textView = findViewById(R.id.textView);
+        imageView = findViewById(R.id.imageViewBackground);
+
+        imageView.setBackgroundColor(getResources().getColor(R.color.backGroundColor));
+        textView.setVisibility(View.INVISIBLE);
+
+        for(Button button:buttons){
+            button.setEnabled(true);
+        }
+
+
+        buttonReload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setBackgroundColor(getResources().getColor(R.color.backGroundColor));
+                textView.setVisibility(View.INVISIBLE);
+                for (Button button : buttons) {
+                    button.setEnabled(true);
+                }
+                createRandomButton();
+            }
+        });
+
+        for (Button button : buttons) {
+            button.setOnClickListener(this);
+        }
+    }
+
+    public Button createRandomButton() {
+        int n = (int) (Math.random() * 6);
+        randomButton = buttons[n];
+        return randomButton;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v.equals(randomButton)) {
+            textView.setVisibility(View.VISIBLE);
+            imageView.setBackgroundColor(getResources().getColor(R.color.newBackGroundColor));
+        } else {
+            v.setEnabled(false);
+        }
+    }
+
+}
